8f38e019afe8cb32e47b02f81f593f0e64d5ac21
longyangking <longyang_123@yeah.net>
2016-08-25 12:06:02 +0800

    Little modification

--- src/Downloader/NetSolver.java
+++ src/Downloader/NetSolver.java
@@ -4,7 +4,10 @@ import java.net.*;
 public class NetSolver {
 	String url;
 	public NetSolver(String url){
-		
+		this.url = url;
 	}
-	
+	public byte[] GetContent(){
+		
+		return null;
+	}
 }
--- src/Downloader/SystemSets.java
+++ src/Downloader/SystemSets.java
@@ -1,6 +1,9 @@
 package Downloader;
 
 public class SystemSets {
+	/**
+	 * The downloading target path
+	 */
 	static String DownloadPath = "";
 	
 }
